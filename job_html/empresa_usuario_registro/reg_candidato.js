dc7c798f5ecf132db9a5730a4c95af2c6dc80a37
joaoreboucas05 <161169964+joaoreboucas05@users.noreply.github.com>
2024-11-06 14:19:15 -0400

    mudei

---
+++ job_html/empresa_usuario_registro/reg_candidato.js
@@ -0,0 +1,7 @@
+document
+        .getElementById("candidateForm")
+        .addEventListener("submit", function (e) {
+          e.preventDefault();
+          // Aqui você pode adicionar a lógica para enviar os dados do formulário
+          alert("Formulário enviado com sucesso!");
+        });
