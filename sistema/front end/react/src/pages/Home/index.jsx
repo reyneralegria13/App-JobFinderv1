da0e56d27399961d66ccfec2c057aa0549d444b3
joaoreboucas05 <161169964+joaoreboucas05@users.noreply.github.com>
2024-11-11 16:31:42 -0400

    Tela inicial

--- sistema/front end/react/src/pages/Home/index.jsx
+++ sistema/front end/react/src/pages/Home/index.jsx
@@ -6,10 +6,13 @@ function Home() {
   return (
     
     <div>
+      <header class="header">
       <div class="logo">JobFinder</div>
+        
+    </header>
 
-      <div class="container">
-        <div className='content'>
+    <main class="container">
+      <div class="content">
         <h1>Bem-vindo ao JobFinder</h1>
         <p>
           No JobFinder, conectamos você com as melhores oportunidades de emprego
@@ -21,20 +24,21 @@ function Home() {
           <a
             href="./registro.html"
             class="button primary-button"
-            >Cadastre-se</a>
+            >Cadastre-se</a
+          >
           <a 
             href="../Entrar/Entrar.html" 
             class="button secondary-button"
             >Entrar</a>
-          </div>
-          <div class="hero-image">
-            <img
-              src="http://127.0.0.1:5500/sistema/front%20end/job_html/img/WhatsApp%20Image%202024-10-23%20at%2015.42.26.jpeg"
-              alt="Pessoas trabalham em um ambiente de escritório moderno"
-            />
-          </div>
         </div>
-    </div>
+      </div>
+      <div class="hero-image">
+        <img
+          src="http://127.0.0.1:5500/sistema/front%20end/job_html/img/WhatsApp%20Image%202024-10-23%20at%2015.42.26.jpeg"
+          alt="Pessoas trabalham em um ambiente de escritório moderno"
+        />
+      </div>
+      </main>
     </div>
   )
 }
