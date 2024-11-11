8339c1459a1e7baaa72515c39d074d23edaa6e6d
joaoreboucas05 <161169964+joaoreboucas05@users.noreply.github.com>
2024-11-11 17:32:53 -0400

    escolher cargo

--- sistema/front end/react/src/main.jsx
+++ sistema/front end/react/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Home from './pages/EsquecerSenha/index'
+import Home from './pages/EscolherCargo/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ sistema/front end/react/src/pages/EscolherCargo/index.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './style.css'
+
+function Home() {
+    return (
+    <div>
+        <header class="header">
+
+            <div class="navigation">
+            <button class="back-button" id="botaovoltar" onclick="window.location.href = './login.html';">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path d="M19 12H5M12 19l-7-7 7-7" />
+          </svg>
+        </button>
+            </div>
+
+            <div class="logo">JobFinder</div>
+
+            
+    </header>
+
+    <main class="container">
+      <h1>Registro</h1>
+      <p>
+        Bem-vindo ao JobFinder! Por favor, escolha se você está procurando um
+        emprego ou se você é um empregador à procura de candidatos. Esta
+        informação nos ajudará a personalizar sua experiência.
+      </p>
+
+      <form id="register-form">
+        <div class="options">
+          <label class="option selected">
+            <input type="radio" name="user-type" value="candidate" checked />
+            <div class="option-content">
+              <div class="option-title">Candidato</div>
+              <div class="option-description">Estou procurando um emprego</div>
+            </div>
+          </label>
+
+          <label class="option">
+            <input type="radio" name="user-type" value="employer" />
+            <div class="option-content">
+              <div class="option-title">Empregador</div>
+              <div class="option-description">Estou procurando candidatos</div>
+            </div>
+          </label>
+        </div>
+
+        <button type="submit" class="continue-button">Continuar</button>
+      </form>
+    </main>
+     </div>       
+                   
+    )
+}
+export default Home
