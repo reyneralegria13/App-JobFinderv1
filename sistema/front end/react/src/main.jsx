f18ca514176cdaef458653a6307dc03041f64dfb
joaoreboucas05 <161169964+joaoreboucas05@users.noreply.github.com>
2024-11-12 10:45:47 -0400

    hfhgarou

--- sistema/front end/react/src/main.jsx
+++ sistema/front end/react/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Home from './pages/Home/index.jsx'
+import Home from './pages/Empregador/Registro/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- sistema/front end/react/src/pages/Empregador/RegistroEmpresa/index.jsx
+++ sistema/front end/react/src/pages/Empregador/RegistroEmpresa/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styleReg.css';
+import './style.css';
 
 function CompanyRegistration() {
   return (
