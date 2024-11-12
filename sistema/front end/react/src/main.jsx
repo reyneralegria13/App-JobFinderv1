f265c9ea0b68d421c9e18c581d6a16423a571680
João Carlos <jcarlosguimaraes13@gmail.com>
2024-11-12 15:58:11 -0400

    Ajuste no import de mainRoutes

--- sistema/front end/react/src/main.jsx
+++ sistema/front end/react/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import mainRoutes from './routes'
+import mainRoutes from './routes/mainRoutes'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- sistema/front end/react/src/pages/Home/index.jsx
+++ sistema/front end/react/src/pages/Home/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
 import './style.css'
 import imagem from '../../img/img_principal.jpeg'
 
