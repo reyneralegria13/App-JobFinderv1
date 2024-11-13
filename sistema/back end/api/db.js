35203e8e10ee7d646473f48ea7ffcc6cb082ff9d
João Carlos <jcarlosguimaraes13@gmail.com>
2024-11-13 12:52:17 -0400

    Update db.js

--- sistema/back end/api/db.js
+++ sistema/back end/api/db.js
@@ -3,5 +3,3 @@ const mongoose =require('mongoose')
 const dbUri = "mongodb+srv://JobFinderDB:WiMuOlRTRqOZUQ5o@jobfinder-server.pgslx.mongodb.net/JobFinder-Server?retryWrites=true&w=majority&appName=JobFinder-Server"
 
 module.exports = () => mongoose.connect(dbUri)
-
-//ok
