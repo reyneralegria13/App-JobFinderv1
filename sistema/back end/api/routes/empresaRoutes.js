3e52bffad6237851e9467ee2d26e0c713112d59c
joaoreboucas05 <161169964+joaoreboucas05@users.noreply.github.com>
2024-11-14 13:40:04 -0400

    Fui  moleque

--- sistema/back end/api/routes/empresaRoutes.js
+++ sistema/back end/api/routes/empresaRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {getEmpresa, getEmpresas, createEmpresa, updateEmpresa, deleteEmpresa} = require("../contrroller/empresaController");
 
 // rotas para cada controle
+router.get('/', (req, res) => res.send('O que tu quer tá mole'));
 router.get('/empresas', getEmpresas);
 router.get("/empresa/:id", getEmpresa);
 router.post("/regEmpresa", createEmpresa);
