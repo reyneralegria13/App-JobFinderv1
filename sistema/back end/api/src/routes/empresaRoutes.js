889aaf1dded499ef05eeda218851bf89a20ae249
Peu <jvggaldino11@gmail.com>
2024-11-15 18:35:09 -0400

    Autenticação de senha a nível backend feita
    Falta realizar essa autenticação junto com o nível de frontend

--- sistema/back end/api/src/routes/empresaRoutes.js
+++ sistema/back end/api/src/routes/empresaRoutes.js
@@ -1,4 +1,8 @@
 const express = require("express");
+//autenticação de senha
+require('dotenv').config()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const router = express.Router();
 const Candidato = require("../models/candidatoModel");
 const { getEmpresa, getEmpresas, createEmpresa, updateEmpresa, deleteEmpresa } = require("../contrroller/empresaController");
@@ -29,6 +33,41 @@ router.get('/cadCandidato', (req, res) => {
 //validação de cadastro de candidato
 router.post('/cadCandidato', async (req, res) => {
   const { nome, cpf, email, senha, telefone, educacao, qualificacao, cursos, descricao, habilidadesTecnicas, idiomas } = req.body;
+
+  //Da erro ao tentar usar isso. Tô com preguiça de saber o pq.
+  /*if(!name){
+    return res.status(422).json({mgs:"O nome é obrigatório!"})
+  }*/
+  if(!cpf){
+    return res.status(422).json({mgs:"O cpf é obrigatório!"})
+  }
+  if(!telefone){
+    return res.status(422).json({mgs:"O telefone é obrigatório!"})
+  }
+  if(!email){
+    return res.status(422).json({mgs:"O email é obrigatório!"})
+  }
+  if(!senha){
+    return res.status(422).json({mgs:"A senha é obrigatório!"})
+  }
+
+  //Caso seja usado o "confirmar senha"
+  /*if(senha != confirmarSenha){
+      return res.status(422).json({mgs:"As senhas não confere!"})
+  }*/
+
+  //Ver se o usuário existe
+  const userExiste = await Candidato.findOne({email: email})
+
+  if(userExiste){
+    return res.status(422).json({mgs:"Email já utilizado no sistema. Por favor, escolher outro."})
+  }
+
+  //proteção de senha 
+  const salt = await bcrypt.genSalt(12)
+  const senhaHash = await bcrypt.hash(senha, salt)
+
+
 
   try {
     const novoCandidato = new Candidato({
@@ -93,6 +132,50 @@ router.get('/login', (req, res) => {
   });
 });
 
+//rota para validação de login
+router.get('/login', async (req, res) => {
+  const {email, senha} = req.body
+
+  //validação inicial
+  if(!email){
+    return res.status(422).json({mgs:"O email é obrigatório!"})
+  }
+  if(!senha){
+    return res.status(422).json({mgs:"A senha é obrigatório!"})
+  }
+
+  //ver se o usuário é encontrado no banco
+  const user = await Candidato.findOne({email: email})
+
+  if(!user){
+    return res.status(404).json({mgs:"Usuário não encontrado!"})
+  }
+
+  //conferir senha do usuário
+  const checarSenha = await bcrypt.compare(senha, user.senha);
+
+  if(!checarSenha){
+    return res.status(422).json({mgs:"Senha inválida!"})
+  }
+
+  //chegar o token
+  try{
+    const secret = process.env.SECRET
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      secret,
+    )
+
+    res.status(200).json({msg:"Autenticação realizada com sucesso", token})
+  } catch (err){
+    console.erro(error)
+    res.status(500).send("Erro ao validar token")
+  }
+});
+
 //rota para recuperação de senha
 router.get('/recuperar_senha', (req, res) => {
   res.render('fun/esqueciSenha', {
@@ -100,6 +183,38 @@ router.get('/recuperar_senha', (req, res) => {
     style: 'esqueciSenha.css'
   });
 });
+
+
+//Rota privada do perfil do usuário para testes de autenticação
+router.get("/user/:id", checarToken, async (req, res) => {
+  const id = req.params.id
+
+  //checar se o usuário existe
+  const user = await Candidato.findById(id, '-senha')
+
+  if(!user){
+    return res.status(404).send("Usuário não encontrado")
+  }
+
+  res.status(200).json({user})
+})
+
+//função para validar o token
+function checarToken(res, req, next){
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(" ")[1]
+  if(!token){
+    return res.status(401).json({msg: 'Acesso negado!'})
+  }
+  try{
+    const secret =process.env.SECRET
+    jwt.verify(token, secret)
+
+    next()
+  } catch(error){
+    res.status(400).json({msg:"token inválido!"})
+  }
+}
 
 
 // outras rotas
