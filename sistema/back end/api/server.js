6dfa814bed370b771d9ccf4cee47c28e90bfade6
joaoreboucas05 <161169964+joaoreboucas05@users.noreply.github.com>
2024-12-07 17:19:42 -0400

    nada

--- sistema/back end/api/server.js
+++ sistema/back end/api/server.js
@@ -62,7 +62,7 @@ connectDb()
 .then(data => {
     console.log(' >> Banco de dados conectado com sucesso:\n')
     app.listen(3000, () => {
-        console.log(' >> Servidor rodando na porta 3000:\n')
+        console.log(`>> Servidor rodando na porta http://localhost:${3000}/home\n`)
     }).on('error', err =>
         console.log('Erro ao ligar o servidor:\n', err))
 })
