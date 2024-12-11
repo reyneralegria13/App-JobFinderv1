cf570e6a9124355bd2232d9f422a7df147b21f9b
Reyner Alegria <reyneralegria18@gmail.com>
2024-12-11 16:05:58 -0400

    Toma

--- sistema/back end/api/src/routes/candidatoRoutes.js
+++ sistema/back end/api/src/routes/candidatoRoutes.js
@@ -26,7 +26,7 @@ router.get('/:candidatoId/candidaturas', getCandidaturasc)
 
 // Rota para editar perfil
 router.get('/perfil/:candidatoId/editar', visualizarTelaEdicao);
-//router.post('/perfil/:candidatoId/editar', editarPerfilCandidato);
+router.post('/perfil/:candidatoId/editar', editarPerfilCandidato);
 
 
 
