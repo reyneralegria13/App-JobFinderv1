b4864cff7c5429914e596862a58d676b06701acf
Peu <jvggaldino11@gmail.com>
2025-05-10 22:07:13 -0400

    Separação de arquivos do sistema

--- sistema/front end/job_html/empresa_usuario_registro/reg_candidato.js
+++
@@ -1,7 +0,0 @@
-document
-        .getElementById("candidateForm")
-        .addEventListener("submit", function (e) {
-          e.preventDefault();
-          // Aqui você pode adicionar a lógica para enviar os dados do formulário
-          alert("Formulário enviado com sucesso!");
-        });
