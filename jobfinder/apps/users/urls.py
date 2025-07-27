149de18757c58ac18f667d3d1d37b5996a2e0c16
Peu <jvggaldino11@gmail.com>
2025-07-26 20:38:11 -0400

    :sparkles: feat: add urls básicas do sistema

---
+++ jobfinder/apps/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import SignUpChoiceView, CandidateSignUpView, EmployerSignUpView
+
+urlpatterns = [
+    path('signup/', SignUpChoiceView.as_view(), name='signup_choice'),
+    path('signup/candidate/', CandidateSignUpView.as_view(), name='candidate_signup'),
+    path('signup/employer/', EmployerSignUpView.as_view(), name='employer_signup'),
+]
